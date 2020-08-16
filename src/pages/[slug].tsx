96318d9b35a125b3e9373f3d2b39be11da731f10
huskyhoochu <dfg1499@gmail.com>
2020-08-16 21:58:54 +0900

    Styling post page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,5 +1,8 @@
 import { GetServerSideProps } from 'next';
+import Link from 'next/link';
 import { SettingsResponse, Tags, PostOrPage } from '@tryghost/content-api';
+import format from 'date-fns/format';
+import parseISO from 'date-fns/parseISO';
 
 import Layout from '../components/layout';
 import getSettings from './api/settings';
@@ -15,7 +18,33 @@ interface Props {
 const Post: React.FC<Props> = (props: Props) => {
   return (
     <Layout {...props}>
-      <p>{props.post.title}</p>
+      <div className="post">
+        <div
+          className="post__thumbnail"
+          style={{ backgroundImage: `url(${props.post.feature_image})` }}
+        />
+        <div className="zippo-container">
+          <div className="post__meta">
+            <p>{props.post.primary_tag.name}</p>
+            <p className="time">
+              {format(parseISO(props.post.published_at), 'yyyy-MM-dd')}
+            </p>
+          </div>
+          <div className="post__title">
+            <h3>{props.post.title}</h3>
+            <hr />
+          </div>
+          <div
+            className="post__content"
+            dangerouslySetInnerHTML={{ __html: props.post.html }}
+          />
+          <div className="post__nav">
+            <Link href={`/tag/${props.post.primary_tag.slug}`}>
+              <a>List</a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
@@ -29,7 +58,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     filter: 'accent_color:#000000',
   });
 
-  const post = await getPost({ slug: slug as string });
+  const post = await getPost({ slug: slug as string }, { include: 'tags' });
 
   return {
     props: {
