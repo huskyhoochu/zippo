c28a499bde47234f3617200f0681be0137a372d0
huskyhoochu <dfg1499@gmail.com>
2020-09-02 11:26:09 +0900

    Set default display dark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   const display = Cookies.get('display');
 
   if (!display) {
-    Cookies.set('display', 'light', {
+    Cookies.set('display', 'dark', {
       expires: 365,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
